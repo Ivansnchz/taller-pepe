2069b7e6c3cd5c71b5881b252c84cf13b6c4cfcc
Ivansnchz <isanchezj49@gmail.com>
2022-10-26 19:56:10 +0200

    clean

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,18 +1,29 @@
-// Good old Javascript
-function sayHello () {
+
+// ES6
+const sayHello = () => {
     const form = document.querySelector("form");
     console.log("Hello, " + form.elements.hello.value);
     alert("Hello, " + form.elements.hello.value);
-} 
+}
+
+// MORE EXAMPLES:
+
+// Good old Javascript
+/* function sayHello2 () {
+    const form = document.querySelector("form");
+    console.log("Hello, " + form.elements.hello.value);
+    alert("Hello, " + form.elements.hello.value);
+}  */
 
 
-// ES6
-const sayHello2 = () => console.log("Hello") // fat arrow functions
-const sayHello3 = (name) => `Hello, ${name}` // fat arrow (return omitido por ser una sola linea de código)
+
+
+// fat arrow functions
+/* const sayHello3 = (name) => `Hello, ${name}` // fat arrow (return omitido por ser una sola linea de código)
 
 
 const sayHello4 = () => {
     return "Hello" // fat arrow 
 
-}
+} */
 
